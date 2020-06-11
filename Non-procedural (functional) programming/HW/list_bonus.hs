84dbab02619e53c98dbac0c1feff4d7274a70a9d
Lukáš Bastián <luk3bastian@gmail.com>
2020-06-11 20:55:28 +0200

    Styling

--- Non-procedural (functional) programming/HW/list_bonus.hs
+++ Non-procedural (functional) programming/HW/list_bonus.hs
@@ -5,10 +5,7 @@ flatten ((x:xs):vs) = x:flatten (xs:vs)
 
 dezip :: [(a, b)] -> ([a], [b])
 dezip [] = ([], [])
-dezip xs = (map dezipfst xs, map dezipsnd xs)
-dezipfst (a, b) = a
-dezipsnd (a, b) = b
-
+dezip xs = (map fst xs, map snd xs)
 
 rev :: [a] -> [a]
 rev = foldl (flip (:)) []
